bb303234f7d6a644f01560ec1649e86897bf19fe
B4bah <osipovmisha09092006@gmail.com>
2025-09-19 00:24:30 +0300

    added params to zapret config

--- configuration.nix
+++ configuration.nix
@@ -115,6 +115,12 @@
   services.zapret = {
     enable = true;
 
+    params = [
+      "tpws --split-pos=host+1 --oob"
+      "tpws --split-pos=1,sniext+1,host+1,midsld,endhost-1 --fix-seg --disorder"
+      "tpws --split-pos=1,sniext+1,host+1,midsld,endhost-1 --fix-seg --oob --disorder"
+      "nfqws --dpi-desync=multidisorder --dpi-desync-split-pos=1,sniext+1,host+1,midsld-2,midsld,midsld+2,endhost-1"
+    ];
     whitelist = [
       "youtube.com"
       "discord.com"
